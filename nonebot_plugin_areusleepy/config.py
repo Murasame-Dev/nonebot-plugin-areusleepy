66b0ab4ad7d2ef74372d93c1a9d841ec90535405
Murasame <162931251+LeiSureLyYrsc@users.noreply.github.com>
2025-06-01 22:26:10 +0800

    測試 Actions 工作流

--- nonebot_plugin_areusleepy/config.py
+++ nonebot_plugin_areusleepy/config.py
@@ -8,6 +8,6 @@ class Config(BaseModel):
     sleepy_show_details: bool = False  # 是否显示详细信息
 
     # Sleepy 服务配置
-    sleepy_url: str = 'https://sleepy-preview.wyf9.top'  # Sleepy 服务地址
+    sleepy_url: str = 'https://status.0d000721.xin'  # Sleepy 服务地址
     sleepy_timeout: float = 5.0  # 请求超时 (秒)
     sleepy_retries: int = 3  # 请求失败时的重试次数
